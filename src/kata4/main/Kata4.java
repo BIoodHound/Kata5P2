71923ca3ba770774ce0009271e3146dfad4b40fb
gonca <gonca@DESKTOP-UT10VI0>
2018-11-04 12:33:28 +0000

    Kata 4 Versión 2

--- src/kata4/main/Kata4.java
+++ src/kata4/main/Kata4.java
@@ -9,13 +9,28 @@ import kata4.view.MailHistogramBuilder;
 import kata4.view.MailListReader;
 
 public class Kata4 {
-
+    static String fileName;
+    static List<Mail> mailList;
+    static Histogram<String> histogram;
+    static HistogramDisplay histoDisplay;
     public static void main(String[] args) throws IOException {
-        String fileName = "email.txt";
-        List<Mail> mailList = new MailListReader().read(fileName);
-        Histogram<String> histogram = new MailHistogramBuilder().build(mailList);
-        
-        HistogramDisplay histoDisplay = new HistogramDisplay(histogram);
+        input();
+        process();
+        output();
+        execute();
+    }
+    
+    public static void input() throws IOException{
+        fileName = "email.txt";
+        mailList = new MailListReader().read(fileName);
+    }
+    public static void process(){
+        histogram = new MailHistogramBuilder().build(mailList);
+    }
+    public static void output(){
+        histoDisplay = new HistogramDisplay(histogram);
+    }
+    public static void execute(){
         histoDisplay.execute();
     }
     
