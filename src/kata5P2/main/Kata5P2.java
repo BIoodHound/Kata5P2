58327a9890974d9a0fdf35e72c03c75deaea08ea
gonca <gonca@DESKTOP-UT10VI0>
2018-12-04 12:56:35 +0000

    Kata 5 P2 Versión 1

---
+++ src/kata5P2/main/Kata5P2.java
@@ -0,0 +1,37 @@
+package kata5P2.main;
+
+import java.io.IOException;
+import java.util.List;
+import kata5P2.model.Histogram;
+import kata5P2.model.Mail;
+import kata5P2.view.HistogramDisplay;
+import kata5P2.view.MailHistogramBuilder;
+import kata5P2.view.MailListReader;
+
+public class Kata5P2 {
+    static String fileName;
+    static List<Mail> mailList;
+    static Histogram<String> histogram;
+    static HistogramDisplay histoDisplay;
+    public static void main(String[] args) throws IOException {
+        input();
+        process();
+        output();
+        execute();
+    }
+    
+    public static void input() throws IOException{
+        fileName = "email.txt";
+        mailList = new MailListReader().read(fileName);
+    }
+    public static void process(){
+        histogram = new MailHistogramBuilder().build(mailList);
+    }
+    public static void output(){
+        histoDisplay = new HistogramDisplay(histogram);
+    }
+    public static void execute(){
+        histoDisplay.execute();
+    }
+    
+}
